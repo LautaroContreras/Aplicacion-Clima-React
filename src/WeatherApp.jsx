9bf3280ff88f47808d03da2522d98abe768c4789
LautaroContreras <LautaroContreras1998@gmail.com>
2024-06-17 21:56:57 -0300

    Agregando .env para api_key

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,67 +1,11 @@
-/* import { useState } from "react"
-
-export const WeatherApp = () => {
-
-    const urlBase = 'https://api.openweathermap.org/data/2.5/weather'
-    const API_KEY = 'af83734946072a1b3d689db5aa7b125e'
-    const difKelvin = 273.15
-
-    const [ciudad,setCiudad] = useState('')
-    const [dataClima,setDataClima] = useState(null)
-
-    const handleCambioCiudad = (e) =>{
-        setCiudad(e.target.value)
-    }
-
-    const handleSubmit = (e) =>{
-        e.preventDefault()
-        if(ciudad.length > 0)  fetchClima()
-    }
-
-    const fetchClima = async () => {
-        try{
-           
-            const response = await fetch (`${urlBase}?q=${ciudad}&appid=${API_KEY} `)
-            const data = await response.json()
-            setDataClima(data)
-        } catch (error){
-            console.error('Ocurrio el siguiente problema:', error)
-        }
-    }
-
-
-  return (
-    <div className='container'>
-        <h1>Aplicación del Clima</h1>
-
-        <form onSubmit={handleSubmit}>
-            <input 
-            type="text"
-            value={ciudad}
-            onChange={handleCambioCiudad}
-             />
-             <button type="submit">Buscar</button>
-        </form>
-        {
-            dataClima && (
-                <div>
-                    <h2>{dataClima.name}</h2>
-                    <p>Temperatura : {parseInt(dataClima?.main?.temp - difKelvin)}°C </p>
-                    <p>Condición meteorologica : {dataClima.weather[0].description}</p>
-                    <img src={`https://openweathermap.org/img/wn/${dataClima.weather[0].icon}@2x.png`} />
-                </div>
-            )
-        }
-    </div>
-  )
-} */
-
-  import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 export const WeatherApp = () => {
     const urlBase = 'https://api.openweathermap.org/data/2.5/weather';
-    const API_KEY = 'af83734946072a1b3d689db5aa7b125e';
-    const difKelvin = 273.15;
+    const API_KEY = import.meta.env.VITE_API_KEY;
+
+    console.log("API Key:", API_KEY); 
+   
 
     const [ciudad, setCiudad] = useState('');
     const [dataClima, setDataClima] = useState(null);
@@ -162,4 +106,5 @@ export const WeatherApp = () => {
         </div>
     );
 };
+
 
